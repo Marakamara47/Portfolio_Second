08fecba9ed2058c5b1a8a24de3f52a71789bd6ef
Marakamara47 <suhiimacsim@gmail.com>
2020-12-27 21:04:07 +0200

    add slider

---
+++ script/main.js
@@ -0,0 +1,12 @@
+// Owl carousel
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:7500,
+        autoplayHoverPause:true,
+        autoplaySpeed:2300,
+        margin:100,
+    });
+  });
